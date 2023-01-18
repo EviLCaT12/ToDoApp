7a157fce926dc0a81500b3d935ee748b4575e960
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2023-01-18 15:24:20 +1000

    add CreateCard class

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        add.setOnClickListener {
+            val intent = Intent(this, CreateCard::class.java)
+            startActivity(intent)
+        }
     }
 }

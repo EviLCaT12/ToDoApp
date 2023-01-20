0bf7013c2eb831121f6af748b7b7323737d25103
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2023-01-20 16:07:09 +1000

    add room`s func to CreateCard

--- app/src/main/java/com/example/myapplication/CreateCard.kt
+++ app/src/main/java/com/example/myapplication/CreateCard.kt
@@ -3,12 +3,20 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_create_card.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CreateCard : AppCompatActivity() {
+    private lateinit var db: DataBase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_card)
+
+        db = Room.databaseBuilder(
+            applicationContext, DataBase::class.java, "ToDoApp"
+        ).build()
         save_button.setOnClickListener {
             if (create_title.text.toString().trim { it <= ' ' }.isNotEmpty()
                 && create_priority.text.toString().trim { it <= ' ' }.isNotEmpty()
@@ -16,6 +24,9 @@ class CreateCard : AppCompatActivity() {
                 var title = create_title.getText().toString()
                 var priority = create_priority.getText().toString()
                 DataObject.setData(title, priority)
+                GlobalScope.launch{
+                    db.dao().insertTask(Entity(0, title, priority))
+                }
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
